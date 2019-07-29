5e5d1917a3b1b7d43d2f1d03678e6b082132b735
tonemy <1006045923@qq.com>
2019-07-29 15:43:52 +0800

    "test"

--- src/main/java/com/zcs/ChallengeProgramming/Aizu0033.java
+++ src/main/java/com/zcs/ChallengeProgramming/Aizu0033.java
@@ -4,4 +4,7 @@ package com.zcs.ChallengeProgramming;
  * Created by 张超帅 on 2019/7/29.
  */
 public class Aizu0033 {
+    public void main(String [] args) {
+        System.out.println("sd");
+    }
 }
