f5ec677e0cc11bbe38528efe221b9143a10f89a4
tonemy <1006045923@qq.com>
2019-07-30 17:32:57 +0800

    test

--- src/main/java/com/zcs/ChallengeProgramming/Poj3669/Main.java
+++ src/main/java/com/zcs/ChallengeProgramming/Poj3669/Main.java
@@ -1,38 +1,17 @@
+package com.zcs.ChallengeProgramming.Poj3669;
 
-**参考地址:** https://cn.vjudge.net/article/46
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.NoSuchElementException;
 
- 
-###  第一章: 蓄势待发
- - [X]    [~~POJ 1852 Ants~~ ](https://cn.vjudge.net/problem/POJ-1852)
+/**
+ * Created by 张超帅 on 2019/7/30.
+ */
+public class Main {
+    public static void main(String[] args) {
 
-### 第二章：初出茅庐
-1. 深度优先搜索
-
- - [X]  [~~POJ 2386 Lake Counting~~](https://cn.vjudge.net/problem/POJ-2386)
- - [X]   [~~POJ 1979 Red and Black~~](https://cn.vjudge.net/problem/POJ-1979)
- - [X] [~~Aizu 0118~~](https://cn.vjudge.net/problem/Aizu-0118)
- - [X] [~~Aizu 0033 Ball~~](https://cn.vjudge.net/problem/Aizu-0033)
- - [ ] POJ 3009
-2. 宽度优先搜索
- - [X] [~~POJ3984 迷宫问题~~](https://cn.vjudge.net/problem/POJ-3984)
- - [X] [~~Aizu 0558 Cheess~~](https://cn.vjudge.net/problem/Aizu-0558)
- - [ ] POJ 3669
- - [ ] Aizu 0121
- - [ ]   POJ 2718
- - [ ] POJ 3187
- - [ ] POJ 3050
- - [ ] Aizu 0525
-
-
- 
-### 附录: 笔记
-- POJ 1852 Ants : 
-  本题的测试案例中，属于多输入案例，但在每组的第二行数据中，有多少行数据不知道。本来我不经常用Scanner来读取数据，常常使用readLine来读取数据，这次在`不知道多少行只知道多少个时，不知道怎么读取数据`。 
-- POJ 1979 Red and Black:
-  本题，我做的思路是没有问题的，检查了好久一直没找到错误，哎~~~~！一直运行错误。
-### 附录：代码
- - 快速读取数据
-```java
+    }
+}
 class FastScanner {
     private final InputStream in = System.in;
     private final byte[] buffer = new byte[1024];
@@ -92,16 +71,14 @@ class FastScanner {
             b = readByte();
         }
     }
-     
+
     public int nextInt() {
         return (int)nextLong();
     }
-     
+
     public double nextDouble() {
         return Double.parseDouble(next());
     }
-     
- 
 }
-```
- 
+
+
